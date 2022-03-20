163ef144ef758f3adc2d4e5c420a3362039f5f5f
AlmusFuad <almusfuad@gmail.com>
2022-03-20 16:47:29 +0600

    Data visible

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
@@ -11,11 +12,34 @@ function App() {
 
 
 function LoadCountries() {
+
+  const [countries, setCountries] = useState ([]);
+
+  useEffect ( () => {
+    fetch ('https://restcountries.com/v3.1/all')
+    .then(res => res.json())
+    .then(data => setCountries(data));
+  }, 
+  
+  []);
     return ( 
       <div>
-        <h1> Every country of the world </h1> 
+        <h1>Every country of the world </h1> 
+        <p>Total countries here: {countries.length}</p> 
+        {
+          countries.map(country => <Country name= {country.name.common} population= {country.population}></Country> )
+        }
       </div>
     );
+}
+
+function Country (props) {
+  return (
+    <div>
+      <h1>Name: {props.name}</h1>
+      <p>popoulation: {props.population}</p>
+    </div>
+  );
 }
 
 export default App;
